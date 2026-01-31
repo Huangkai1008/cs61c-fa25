dce71b4e2399aa9bc586cd34ed0e064f59a004db
huangkai <h1770360848@outlook.com>
2026-01-31 16:15:25 +0800

    feat(project 1): finish task 7

--- projects/proj1/src/snake.c
+++ projects/proj1/src/snake.c
@@ -1,70 +1,105 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "snake_utils.h"
 #include "game.h"
+#include "snake_utils.h"
 
-int main(int argc, char *argv[]) {
-  bool io_stdin = false;
-  char *in_filename = NULL;
-  char *out_filename = NULL;
-  game_t *game = NULL;
+int main(int argc, char *argv[])
+{
+    bool io_stdin = false;
+    char *in_filename = NULL;
+    char *out_filename = NULL;
+    game_t *game = NULL;
 
-  // Parse arguments
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-i") == 0 && i < argc - 1) {
-      if (io_stdin) {
+    // Parse arguments
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-i") == 0 && i < argc - 1)
+        {
+            if (io_stdin)
+            {
+                fprintf(stderr, "Usage: %s [-i filename | --stdin] [-o filename]\n", argv[0]);
+                return 1;
+            }
+            in_filename = argv[i + 1];
+            i++;
+            continue;
+        }
+        else if (strcmp(argv[i], "--stdin") == 0)
+        {
+            if (in_filename != NULL)
+            {
+                fprintf(stderr, "Usage: %s [-i filename | --stdin] [-o filename]\n", argv[0]);
+                return 1;
+            }
+            io_stdin = true;
+            continue;
+        }
+        if (strcmp(argv[i], "-o") == 0 && i < argc - 1)
+        {
+            out_filename = argv[i + 1];
+            i++;
+            continue;
+        }
         fprintf(stderr, "Usage: %s [-i filename | --stdin] [-o filename]\n", argv[0]);
         return 1;
-      }
-      in_filename = argv[i + 1];
-      i++;
-      continue;
-    } else if (strcmp(argv[i], "--stdin") == 0) {
-      if (in_filename != NULL) {
-        fprintf(stderr, "Usage: %s [-i filename | --stdin] [-o filename]\n", argv[0]);
-        return 1;
-      }
-      io_stdin = true;
-      continue;
     }
-    if (strcmp(argv[i], "-o") == 0 && i < argc - 1) {
-      out_filename = argv[i + 1];
-      i++;
-      continue;
+
+    // Do not modify anything above this line.
+
+    /* Task 7 */
+
+    // Read board from file, or create default board
+    if (in_filename != NULL)
+    {
+        // Load the board from in_filename.
+        //
+        // If the file doesn't exist, return -1.
+        // Then call initialize_snakes on the game you made.
+        // Remember close file pointer.
+        FILE *fp = fopen(in_filename, "r");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "File %s does not exist\n", in_filename);
+            return -1;
+        }
+
+        game = load_board(fp);
+        initialize_snakes(game);
+        fclose(fp);
     }
-    fprintf(stderr, "Usage: %s [-i filename | --stdin] [-o filename]\n", argv[0]);
-    return 1;
-  }
 
-  // Do not modify anything above this line.
+    else if (io_stdin)
+    {
+        // Load the board from stdin.
+        // Then call initialize_snakes on the state you made.
+        game = load_board(stdin);
+        initialize_snakes(game);
+    }
 
-  /* Task 7 */
+    else
+    {
+        game = create_default_game();
+    }
 
-  // Read board from file, or create default board
-  if (in_filename != NULL) {
-    // TODO: Load the board from in_filename
-    // TODO: If the file doesn't exist, return -1
-    // TODO: Then call initialize_snakes on the game you made
-    // TODO: close file pointer
-  } else if (io_stdin) {
-    // TODO: Load the board from stdin
-    // TODO: Then call initialize_snakes on the game you made
-  } else {
-    // TODO: Create default game
-  }
+    // Update state. Use the deterministic_food function
+    // (already implemented in snake_utils.h) to add food.
+    update_game(game, &deterministic_food);
 
-  // TODO: Update game. Use the deterministic_food function
-  // (already implemented in snake_utils.h) to add food.
+    // Write updated board to file or stdout.
+    if (out_filename != NULL)
+    {
+        // Save the board to out_filename
+        save_board(game, out_filename);
+    }
+    else
+    {
+        // Print the board to stdout.
+        print_board(game, stdout);
+    }
 
-  // Write updated board to file or stdout
-  if (out_filename != NULL) {
-    // TODO: Save the board to out_filename
-  } else {
-    // TODO: Print the board to stdout
-  }
+    // Free the game.
+    free_game(game);
 
-  // TODO: Free the game
-
-  return 0;
+    return 0;
 }
